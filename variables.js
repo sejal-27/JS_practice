8e53ea78701a38be14ccb818d37f482a7c154e82
sejal-27 <sejall2709@gmail.com>
2023-08-04 10:54:09 +0530

    variable n datatype updated

---
+++ variables.js
@@ -0,0 +1,21 @@
+//integer
+let x = 10;
+let y = 2;
+let z = 2;
+
+document.write(x + y);
+
+//string
+let name = "sejal";
+const fullname = ["sejal", "thakur"];
+document.write("<br/> name= \t" + name + "<br/> fullname=\t" + fullname);
+
+//boolean
+let m = true;
+let n = false;
+document.write("<br>");
+document.write(m);
+document.write("<br>");
+document.write(x == y);
+document.write("<br>");
+document.write(y == z);
